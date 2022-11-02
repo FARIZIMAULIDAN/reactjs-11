693bfe4bbddad70b1f2119c4cd8d8be10ffcb36d
Your Name <farizimaulidan@gmail.com>
2022-11-02 17:18:46 +0700

    pertemuan11

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+return (
+  <div className="Parentbox">
+  <div className="Foto">
+    <img src="neko.jpg"/>
+  </div>
+  <div className="Deskripsi">
+    <h1 className="Cate">LINK PEMERSATU BANGSA</h1>
+    <h1 className="Title">NEKOPOI CARE</h1>
+    <p className="Price">GRATIS BUAT LELAKI YANG KESEPIAN DI MALAM HARI</p>
+    <p className="Info">
+    Jika kalian para kaum adam kesepian mampirlah ke Website kami ya dukung kita supaya lebih maju.YTTA
+    </p>
+    <div>
+      <button className="vanilla">VANILLA</button>
+      <button className="incest">INCEST</button>
+      <button className='harem'>HAREM</button>
+      <button className="ntr">NTR</button>
     </div>
-  );
+    <button className="pilih">PILIH BEB</button>
+  </div>
+ </div>
+);
 }
+
+
 
 export default App;
